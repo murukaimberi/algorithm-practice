6c2d2e54d6f8e02ce7fafb43028bf6a703ce8ff1
murukai.mberi <murukai.mberi@wonderlabz.com>
2024-06-05 04:55:30 +0200

    up to page 303 QuickSort

--- src/main/java/org/afrikatek/co/za/algorithmspractice/sorting/ThreeWayQuickSort.java
+++ src/main/java/org/afrikatek/co/za/algorithmspractice/sorting/ThreeWayQuickSort.java
@@ -38,29 +38,4 @@ public class ThreeWayQuickSort {
             sort(a, gt + 1, hi);
         }
     }
-
-    private static int partition(Comparable[] a, int lo, int hi) {
-        // Partition into a[lo..i - 1], a[i], a[i + 1..hi].
-        int i = lo, j = hi + 1;
-        Comparable v = a[lo];
-        while (true) {
-            // Scan right, scan left, check for scan complete, and exchange.
-            while(less(a[++i], v)) {
-                if (i == hi) {
-                    break;
-                }
-            }
-            while (less(v, a[--j])) {
-                if (j == lo) {
-                    break;
-                }
-            }
-            if (i >= j) {
-                break;
-            }
-            exchange(a, i, j);
-        }
-        exchange(a, lo, j); // Put v = a[j] into position
-        return j; // with a[lo..j - 1] <= a[j] <= a[j + 1..hi].0
-    }
 }
